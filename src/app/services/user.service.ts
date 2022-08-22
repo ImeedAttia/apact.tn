bfb040664b5444e1066fec6ebe5336728bd7a741
apact <tn.apact@gmail.com>
2022-08-22 18:38:47 +0100

    logo commit

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
-
+import { FirebaseApp } from '@angular/fire/app';
+import {
+  getStorage,
+  ref,
+  listAll
+} from "firebase/storage";
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+storage : any
+  constructor( private firebaseApp : FirebaseApp) {
 
-  constructor() { }
+
+   }
+
+   getImg(){
+    return this.storage = getStorage(this.firebaseApp, "gs://my-custom-bucket");
+   }
+
+   
 }
