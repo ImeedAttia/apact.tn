bfcb3ec5475c00e4887a5f9c45f533974dd15f0c
apact <tn.apact@gmail.com>
2022-08-23 20:01:02 +0100

    remove sppinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { UserService } from './services/user.service';
 import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
 import { ErrorStateMatcher, ShowOnDirtyErrorStateMatcher } from '@angular/material/core';
 import { FooterComponent } from './components/nav-footer/footer/footer.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
   declarations: [
@@ -46,7 +47,9 @@ import { FooterComponent } from './components/nav-footer/footer/footer.component
     HttpClientModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore())
+    provideFirestore(() => getFirestore()),
+    NgxSpinnerModule,
+    BrowserAnimationsModule
   ],
   providers: [
     DataService,
