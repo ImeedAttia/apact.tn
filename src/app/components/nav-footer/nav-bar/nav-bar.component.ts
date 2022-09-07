ebf9029a2170e6207297793de2c2c4b0dc6bde9b
apact <tn.apact@gmail.com>
2022-09-07 15:48:51 +0100

    fix navbar isuues

--- src/app/components/nav-footer/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-footer/nav-bar/nav-bar.component.ts
@@ -9,37 +9,38 @@ import { AuthService } from 'src/app/services/auth.service';
 export class NavBarComponent implements OnInit {
 
 
-//Declaration
-  //Path of logo img
-  LogoImgPath="../../../assets/logo.png"
-  // User Status
-  isLoggedIn : boolean = false;
-  //usr data
-  userData : any ={};
-  constructor(private authService: AuthService) { }
+  //Declaration
+    //Path of logo img
+    LogoImgPath="../../../assets/logo.png"
+    // User Status
+    isLoggedIn : boolean = false;
+    //usr data
+    userData : any ={};
+
+    
+    constructor(private authService: AuthService) { }
 
 
-  ngOnInit(): void {
-    if(this.isLoggedIn = !!this.authService.getAuthLocal()){
-      this.refreshProfile();
+    ngOnInit(): void {
+      if(this.isLoggedIn = !!this.authService.getAuthLocal()){
+        this.refreshProfile();
+      }
     }
 
+    refreshProfile(){
+      this.userData = this.authService.getAuthLocal();
 
-  }
-
-  refreshProfile(){
-    this.userData = this.authService.getAuthLocal();
-
-    this.userData = {
-      "displayName" : this.userData.displayName ? this.authService.getAuthLocal().displayName : "",
-      "photoURL" : this.userData.providerData[0].photoURL ? this.userData.providerData[0].photoURL :   "https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp"
+      this.userData = {
+        "displayName" : this.userData.displayName ? this.authService.getAuthLocal().displayName : "",
+        "photoURL" : this.userData.providerData[0].photoURL ? this.userData.providerData[0].photoURL :   "https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp"
+      }
     }
-}
-  //Method to logout
-  logout(){
-    this.authService.logout();
-    this.isLoggedIn = false;
-  }
+
+    //Method to logout
+    logout(){
+      this.authService.logout();
+      this.isLoggedIn = false;
+    }
 
 
 }
